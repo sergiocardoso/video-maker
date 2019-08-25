9ec15418ec7eae1a4a636bb6a36f22633e9f05be
Sérgio Cardoso <sergiointernet@gmail.com>
2019-08-25 11:59:03 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const readline = require('readline-sync');
 const robots = {
     input: require('./robots/input'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 };
 
 async function start() {
 
     robots.input()
     await robots.text();
+    await robots.image();
 
     const content = robots.state.load()
     console.dir(content, {
